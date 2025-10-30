442ec0fb19c898533adf9dd6eb5bcfcda608882a
MonkeySaint <monekysaint@gmail.com>
2025-10-29 22:27:39 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,146 @@
+﻿// Author:  Kyle Chapman and Kyle Vardy
+// Created: October 1, 2025
+// Updated: October 29, 2025
+// Description:
+// Code for a WPF form to display car objects. Currently, it is purely for
+// display, and shows a car's make, model, year and price. Creating the car
+// class to support the function of this program is meant as an exercise.
+// Functionality should exist to move through the list of car objects.
+// See the Car class (which you will create in a separate file!) for more details.
+
+namespace CarList
+{
+    /// <summary>
+    /// Stores info on cars in memory
+    /// </summary>
+    internal class Car
+    {
+        // setup local variables
+        private static int count;
+        private readonly int id;
+        private string make;
+        private string model;
+        private int year;
+        private decimal price;
+        private bool isNew;
+
+        /// <summary>
+        /// Fill all the variables about the car
+        /// </summary>
+        /// <param name="make">The make of the car</param>
+        /// <param name="model">The model of the car</param>
+        /// <param name="year">The year the car was made</param>
+        /// <param name="price">The price of the car</param>
+        /// <param name="isNew">If the car is used or not</param>
+        public Car(string make, string model, int year, decimal price, bool isNew)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+            this.isNew = isNew;
+        }
+
+        /// <summary>
+        /// Getter and setter for the IsNew variable 
+        /// </summary>
+        public bool IsNew
+        {
+            get
+            {
+                return isNew;
+            }
+
+            set
+            {
+                isNew = value;
+            }
+        }
+
+        /// <summary>
+        /// Getter for the id variable 
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// Getter and setter for the make variable 
+        /// </summary>
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+
+            set
+            {
+                make = value;
+            }
+        }
+
+        /// <summary>
+        /// Getter and setter for the Model variable 
+        /// </summary>
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                model = value;
+            }
+        }
+
+        /// <summary>
+        /// Getter and setter for the Year variable 
+        /// </summary>
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                year = value;
+            }
+        }
+
+        /// <summary>
+        /// Getter and setter for the Price variable 
+        /// </summary>
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        /// <summary>
+        /// Static getter for the Count variable 
+        /// </summary>
+        public static int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+    }
+}
